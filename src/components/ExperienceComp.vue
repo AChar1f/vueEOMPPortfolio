6a4dd56855002c58b95ff2dcb51c2d5e225d8087
AChar1f <acharif41@gmail.com>
2024-07-01 15:39:46 +0200

    experience section added

---
+++ src/components/ExperienceComp.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container XP">
+        <div class="row">
+            <h4 class="display-4">
+                Experience
+            </h4>
+        </div>
+        <div class="row">
+            <div class="col" v-for="(content, id) in Experience" :key="id">
+                <h4>{{content.role}}</h4>
+                <h4>{{ content.location }}</h4>
+                <h5>{{ content.duration }}</h5>
+                <p>{{ content.description }}</p>
+            </div>
+        </div>    
+    </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+
+
+const store = useStore()
+const Experience = computed(() => store.state.experience)
+onMounted(() => store.dispatch('fetchExperience'))
+</script>
+
+<style scoped>
+    .XP{
+        border: 3px solid white;
+    }
+</style>
