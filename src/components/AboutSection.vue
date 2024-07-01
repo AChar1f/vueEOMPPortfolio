86fc67a28b5c13ca23a9183b21f6526c390a60cf
AChar1f <acharif41@gmail.com>
2024-07-01 13:40:14 +0200

    about section added

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row  text align-items-center">
+            <h2 class="display-2">About</h2>
+            <div v-if="about?.length" class="col">
+                <p>
+                {{ about[0]?.part1 }}
+                </p>
+                <p>
+                    {{ about[0]?.part2 }}
+                </p>
+                <p>
+                    {{ about[0]?.part3 }}
+                </p>            
+            </div>
+            <Spinner v-else/>
+        </div>
+        
+    </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+import Spinner from './SpinnerComp.vue'
+
+const store = useStore()
+const about = computed(() => store.state.about)
+
+onMounted(() => {
+    store.dispatch('fetchAbout')
+})
+</script>
+
+<style scoped>
+
+</style>
