c92e33c0e16debd22cece6471ceb043c47e76d43
AChar1f <acharif41@gmail.com>
2024-07-01 11:31:19 +0200

    footer component added

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,16 @@
     <main class="container-fluid">
       <router-view />
     </main>
+    <Footer/>
   </div>
 </template>
 
 <script>
   import Navbar from '@/components/NavbarComp.vue'
-
+  import Footer from '@/components/FooterComp.vue'
   export default {
     components: {
-      Navbar
+      Navbar, Footer
     }
   }
 </script>
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,25 @@
+<template >
+    <footer class="container-fluid">
+        <div class="row">
+            <p class="lead">Abdurahmaan's Portfolio</p>
+            <span>&copy; {{currentYear}}</span>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: 'FooterComp',
+    data() {
+        return {
+            currentYear : new Date().getUTCFullYear()
+        }
+    }
+}
+</script>
+<style scoped>
+    .container-fluid {
+        color: whitesmoke;
+        border-top: 4px solid #A29D95;
+        background-image: linear-gradient(180deg, black 50%,#232323)
+    }
+</style>
